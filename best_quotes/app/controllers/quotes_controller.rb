1cfcee3b0f392aa4ee6965ccf02f471b55232559
Kevin Staiger <kstag07@yahoo.com>
2014-06-22 20:26:58 -0700

    turning in chapter 6

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,21 +1,23 @@
 class QuotesController < Rulers::Controller
-  def a_quote
-    render :a_quote, :noun => :winking
-  end
 
-
-  def exception
-    raise "It's a bad one!"
-  end
-
-  def quote_1
-    quote_1 = Rulers::Model::FileModel.find(1)
-    render :quote, :obj => quote_1
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
   end
 
   def index
     quotes = FileModel.all
     render :index, :quotes => quotes
+  end
+
+  def a_quote
+    render :a_quote, :noun => :winking
+  end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
   end
 
   def new_quote
@@ -27,4 +29,5 @@ class QuotesController < Rulers::Controller
     m = FileModel.create attrs
     render :quote, :obj => m
   end
+
 end
